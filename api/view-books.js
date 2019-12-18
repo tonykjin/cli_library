e760f8c95b1ad234b13c9bdbe56d9f6eb97ca957
Tony Jin <tonyjin23@gmail.com>
2019-12-17 17:05:40 -0800

    created view all timeline

--- api/view-books.js
+++ api/view-books.js
@@ -1,5 +1,25 @@
-const express = require('express');
+const inquirer = require('inquirer');
+const filter = require('../lib/filter');
 
-module.exports = () => {
-  const router = express.Router();
+module.exports = async ( lib ) => {
+  const result = await filter( lib );
+  inquirer
+    .prompt([
+      {
+        type: 'rawlist',
+        name: 'view',
+        message: '==== View Books ====',
+        choices: [... result],
+        default: false
+      }
+    ])
+    .then((answers) => {
+      console.log('answers:', answers);
+      const book = lib.find((index) => {
+        if (index.title == answers.view) {
+          return index;
+        };
+      });
+      console.log(book);
+    })
 }
--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,36 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
 
-inquirer
-  .prompt([
-    {
-      type: 'rawlist',
-      name: 'main',
-      message: '==== Book Manager ====',
-      choices: [
-        'View all books',
-        'Add a book',
-        'Edit a book',
-        'Search for a book',
-        'Save and Exit'
-      ],
-      default: false
-    }
-  ])
-  .then((answers) => {
-    console.log(JSON.stringify(answers, null, '    '));
-    console.info('Answers var: ', answers);
-    // (answers === 'View all books') ? () :
-  });
+const viewBooks = require('../api/view-books');
+const lib = require('../api/books.json');
+
+(async () => {
+  try {
+    inquirer
+      .prompt([
+        {
+          type: 'rawlist',
+          name: 'main',
+          message: '==== Book Manager ====',
+          choices: [
+            'View all books',
+            'Add a book',
+            'Edit a book',
+            'Search for a book',
+            'Save and Exit'
+          ],
+          default: false
+        }
+      ])
+      .then((answers) => {
+        const { main } = answers;
+        if (main == 'View all books') {
+          viewBooks( lib );
+        };
+      });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
